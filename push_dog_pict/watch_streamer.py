be89a498aab52b3a95a6dba71a2a73cc58af5019
fumi-san <fumisan.pub@gmail.com>
2018-08-16 14:45:17 +0900

    Fix methods not to use user stream
    According to Twitter official's fuck'n announcement,
    user stream is no longer available.
    Get timelines by using user api.

--- push_dog_pict/watch_streamer.py
+++ push_dog_pict/watch_streamer.py
@@ -3,11 +3,13 @@
 from twitter_connect.create_streamer import CreateStreamer
 from twitter_connect.tweet_pict import TweetPict
 from twitter_connect.create_session import CreateSession
+from twitter_connect.get_timeline import GetTimeline
 from pi_camera.exec_picamera import ExecPicamera
 import configparser
 from datetime import datetime
 import json
 import subprocess
+import time
 
 def post_photo(msg='', pict_path=''):
     session = CreateSession(param_account).create_session()
@@ -46,73 +48,105 @@ if __name__ == '__main__':
         param_camera['movie_time'] = 5
 
     # set param_connect
-    if param_connect.get('reconnect_streamer') is None:
-        param_connect['reconnect_streamer'] = 600
+    if param_connect.get('get_timeline_delay') is None:
+        param_connect['get_timeline_delay'] = 70
+    if param_connect.get('reconnect_time') is None:
+        param_connect['reconnect_time'] = 6
 
     # create camera
     import picamera
     camera_init = picamera.PiCamera()
 
-    # create streamer
-    streamer = CreateStreamer(param_account).create_streamer()
-    if (streamer.status_code != 200):
+    # get the newest timeline-id if exist
+    session = CreateSession(param_account).create_session()
+    gettimeline = GetTimeline(session)
+    for i in range(int(param_connect.get('reconnect_time'))):
+        res = gettimeline.get_timeline(count=1)
+        if (res.status_code == 200):
+            break
+        print("Retry for the first connect.")
+        time.sleep(int(param_connect.get('get_timeline_delay')))
+    if (res.status_code != 200):
         print("Err: http status is %s."%(res.status_code))
         exit(1)
-
-    # memorize create_conn date
-    tmp_time = datetime.now()
+    timeline = json.loads(res.text)
+    if len(timeline) == 1:
+        tmp_since_id = timeline[0].get('id')
+    elif len(timeline) != 1 or tmp_since_id is None:
+        tmp_since_id = ''
 
     # loop until killed
-    for line in streamer.iter_lines():
-        if(len(line) == 0):
+    while True:
+        # create session
+        session = CreateSession(param_account).create_session()
+        gettimeline = GetTimeline(session)
+        if tmp_since_id == "":
+            res = gettimeline.get_timeline(count=20)
+        else:
+            res = gettimeline.get_timeline(count=20,
+                                           since_id=tmp_since_id)
+        # wait for the next iter if status_code is illegal
+        if (res.status_code != 200):
+            print("Err: http status is %s."%(res.status_code))
+            print("Sleep for the sleeping time.")
+            time.sleep(int(param_connect.get('get_timeline_delay')))
             continue
-        timeline = json.loads(line.decode('utf-8'))
-        text = timeline.get('text')
-        if text is None:
+
+        timeline = json.loads(res.text)
+        if len(timeline) == 0:
+            print("Sleep before the next read.")
+            time.sleep(int(param_connect.get('get_timeline_delay')))
             continue
 
-        # check if there's words for photo
-        watch_flag = False
-        for w in watch_text_photo:
-            if w in text:
-                watch_flag = True
+        # renew the newest id
+        tmp_since_id = timeline[0].get('id')
+
+        for tm in timeline:
+            text = tm.get('text')
+
+            if text is None:
+                time.sleep(int(param_connect.get('get_timeline_delay')))
+                continue
+
+            # check if there's words for photo
+            watch_flag_photo = False
+            for w in watch_text_photo:
+                if w in text:
+                    watch_flag_photo = True
+                    break
+            # taking a picture if some words exist.
+            if watch_flag_photo:
+                camera = ExecPicamera(camera_init,param_camera)
+                filename = datetime.today().strftime("%Y%m%d_%H%M%S")
+                camera.take_pict('photo/' + filename + '.jpg')
+                post_photo(filename, 'photo/' + filename + '.jpg')
+                rm = subprocess.call(['rm', 'photo/' + filename + '.jpg'])
+
+            # check if there's words for movie
+            watch_flag_movie = False
+            for w in watch_text_movie:
+                if w in text:
+                    watch_flag_movie = True
+                    break
+            # taking a movie if some words exist.
+            if watch_flag_movie:
+                camera = ExecPicamera(camera_init,param_camera)
+                filename = datetime.today().strftime("%Y%m%d_%H%M%S")
+                camera.take_movie('movie/' + filename + '.h264',
+                            param_camera.get('movie_time'))
+                conv = subprocess.call(['/usr/bin/MP4Box', '-fps', '20',
+                           '-add', 'movie/' + filename + '.h264',
+                           'movie/' + filename + '.mp4'])
+                post_movie(filename, 'movie/' + filename + '.mp4')
+                rm = subprocess.call(['rm', 'movie/' + filename + '.mp4'])
+                rm2 = subprocess.call(['rm', 'movie/' + filename + '.h264'])
+
+            # check if it needs to go to the next iter
+            if watch_flag_photo or watch_flag_movie:
+                print("Finished output files")
                 break
-        # taking a picture if some words exist.
-        if watch_flag:
-            camera = ExecPicamera(camera_init,param_camera)
-            filename = datetime.today().strftime("%Y%m%d_%H%M%S")
-            camera.take_pict('photo/' + filename + '.jpg')
-            post_photo(filename, 'photo/' + filename + '.jpg')
-            rm = subprocess.call(['rm', 'photo/' + filename + '.jpg'])
 
-        # check if there's words for movie
-        watch_flag = False
-        for w in watch_text_movie:
-            if w in text:
-                watch_flag = True
-                break
-        # taking a movie if some words exist.
-        if watch_flag:
-            camera = ExecPicamera(camera_init,param_camera)
-            filename = datetime.today().strftime("%Y%m%d_%H%M%S")
-            camera.take_movie('movie/' + filename + '.h264',
-                        param_camera.get('movie_time'))
-            conv = subprocess.call(['/usr/bin/MP4Box', '-fps', '20',
-                       '-add', 'movie/' + filename + '.h264',
-                       'movie/' + filename + '.mp4'])
-            post_movie(filename, 'movie/' + filename + '.mp4')
-            rm = subprocess.call(['rm', 'movie/' + filename + '.mp4'])
-            rm2 = subprocess.call(['rm', 'movie/' + filename + '.h264'])
-
-        # refresh the connection if time is over.
-        if (datetime.now() - tmp_time).total_seconds()\
-                > int(param_connect.get('reconnect_streamer')):
-            # create streamer
-            streamer = CreateStreamer(param_account).create_streamer()
-            if (streamer.status_code != 200):
-                print("Err: http status is %s."%(res.status_code))
-                exit(1)
-
-            # memorize create_conn date
-            tmp_time = datetime.now()
+        # sleep afterall
+        print("sleep before the next check")
+        time.sleep(int(param_connect.get('get_timeline_delay')))
 
